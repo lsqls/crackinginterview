4047a4788f55dcf2ac062b3dd3f63baf4e9c2df7
lbs <764666894@qq.com>
2021-09-03 10:20:20 +0800

    判定字符是否唯一

---
+++ CI1.java
@@ -0,0 +1,13 @@
+class CI1{
+    public boolean isUnique(String astr) {
+        // 26 个小写字母，可以使用一个int进行位运算， 如果是128个字符，可以使用4个int进行位运算
+        int mask=0;
+        for(int i=0;i<astr.length();i++){
+            int bit=1 << (astr.charAt(i)-'a');
+            if((mask & bit) !=0)
+                return false;
+            mask = mask | bit;
+        }
+        return true;
+    }
+}
