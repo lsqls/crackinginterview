383166966c5308f0c5c612ce3792dfffdbb48889
lbs <764666894@qq.com>
2021-09-25 20:41:39 +0800

    移除重复节点

--- chapter2/CI1.java
+++ chapter2/CI1.java
@@ -1,8 +1,28 @@
 package chapter2;
+import java.util.HashSet;
 
+import lib.leetcode.ListNode;
 public class CI1 {
     public ListNode removeDuplicateNodes(ListNode head) {
 
+        if(head==null)
+            return null;
+        HashSet<Integer> iSet=new HashSet<>();
+        ListNode cur=head,next=cur.next;    
+        iSet.add(cur.val);
+        while(cur!=null&&next!=null){
+            while(next!=null&&iSet.contains(next.val)){
+                next=next.next;
+            }
+            
+            cur.next=next;
+            cur=next;
+            if(cur!=null){
+                next=cur.next;
+                iSet.add(cur.val);
+            }
+        }
+        return head;
     }
     
 }
